6e4d86dcd0247ecf02114e29ee0fd247db915b2c
venyao <ywhzau@gmail.com>
2015-11-09 18:34:35 +0800

    update the homepage

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ RICENCODE
 
 The knowledge of cloned rice genes lost in the information of rice functional genomic studies
 
-We collected 2204 cloned rice genes and 303 gene families comprised of 3381 genes. For each gene, 
+We collected 2500 cloned rice genes and 310 gene families comprised of 3691 genes. For each gene, 
 various information concerning the gene symbol, the genomic locus in the Nipponbare reference genome, 
 the Genbank accession number, the title, journal, year, author affiliation, abstract of related 
 publications and the text mining result of the publications were provided.
--- ui.R
+++ ui.R
@@ -25,8 +25,8 @@ shinyUI(fluidPage(theme="mystyle.css",
       p(HTML("
 			 <h3><span style='font-family: Comic Sans MS'>Introdution</span></h3>
              <div align='justify' style='font-family: Comic Sans MS'>
-                   More than 2204 cloned rice genes and 303 gene families 
-                   comprised of 3381 genes were collected. For each gene, various information 
+                   More than 2500 cloned rice genes and 310 gene families 
+                   comprised of 3691 genes were collected. For each gene, various information 
                    concerning the gene symbol, the genomic locus in the 
                    Nipponbare reference genome, the Genbank accession number, 
                    the title, journal, year, author affiliation, abstract of 
