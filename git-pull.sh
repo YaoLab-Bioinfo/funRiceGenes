9f7969a44427713f76aa239cca666b0d6304c667
andrewhzau <ywhzau@gmail.com>
2016-03-02 11:04:55 +0800

    pdf and pdf symbol link

---
+++ git-pull.sh
@@ -0,0 +1,6 @@
+
+# choose 'open with': C:\Program Files\Git\bin\sh.exe
+git pull
+echo 'Press enter to exit'
+read junk
+
--- script/createLinks4pdf.R
+++ script/createLinks4pdf.R
@@ -1,25 +1,44 @@
 
-# library(BBmisc)
+library(BBmisc)
 
 setwd("~")
 all.pdfs <- list.files("E:/坚果云/GIT/RICENCODE/data/Gene", 
                        patter="*.pdf$", full=T, recur=T)
+all.pdfs <- gsub("-冲突-andrew", "", all.pdfs)
 length(all.pdfs)
 all.pdfs[1:5]
 length(unique(basename(all.pdfs)))
 
-# getRelativePath(to="E:/坚果云/GIT/RICENCODE/data/Gene/Abstract/0-9/alpha1a/J. Biol. Chem.-2001-Jiang-9322-9.pdf", 
-#                from="E:/坚果云/GIT/RICENCODE/data/Gene/Abstract/0-9/alpha1b/J. Biol. Chem.-2001-Jiang-9322-9.pdf")
+ct.fls <- all.pdfs[grepl("冲突", all.pdfs)]
+str(ct.fls)
+ct.fls.rt <- gsub("-冲突-andrew", "", ct.fls)
+str(ct.fls.rt)
+file.remove(ct.fls.rt)
+file.rename(ct.fls, ct.fls.rt)
 
-all.pdfs.df <- data.frame(all.pdfs, basename(all.pdfs), stringsAsFactors = FALSE)
+getRelativePath(to="E:/坚果云/GIT/RICENCODE/data/Gene/Abstract/0-9/alpha1a/JBC-2001-Jiang-9322-9.pdf", 
+               from="E:/坚果云/GIT/RICENCODE/data/Gene/Abstract/0-9/alpha1b/JBC-2001-Jiang-9322-9.pdf")
+
+all.pdfs.df <- data.frame(all.pdfs, basename(all.pdfs), file.size(all.pdfs),
+                          stringsAsFactors = FALSE)
 library(plyr)
-names(all.pdfs.df) <- c("file", "pdf")
-d_ply(all.pdfs.df[11:nrow(all.pdfs.df), ], .(pdf), function(df){
+names(all.pdfs.df) <- c("file", "pdf", "size")
+d_ply(all.pdfs.df, .(pdf), function(df){
   if (nrow(df)>=2) {
-    for (i in 2:nrow(df)) {
-      unlink(df$file[i])
-      file.symlink(from=df$file[1], to=df$file[i])
+    froms <- df$file[df$size==0]
+    tos <- df$file[df$size>0]
+    if (length(froms)>0 && length(tos)>0) {
+      for (i in froms) {
+        file.remove(i)
+        from.pt <- getRelativePath(to=tos[1], from=i)
+        from.pt <- sub("\\.\\.\\/", "", from.pt)
+        to.path <- dirname(i)
+        setwd(to.path)
+        file.symlink(from=from.pt, to=".")
+        setwd("~")
+      }
     }
   }
 })
+
 
