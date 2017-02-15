5217d995f93d661ff027e9cab8496766596fc3d5
venyao <ywhzau@gmail.com>
2017-02-15 19:13:26 +0800

    add new family: tmpp

--- active.js
+++ active.js
@@ -2,21 +2,16 @@
 $(document).ready(function() {
 
     /* clear file button control */
-    var fileControl = $("#file1");
+    var fileControl = $("#genfamin");
 
-    $("#clearFile1").on("click", function () {
+    $("#clear6").on("click", function () {
         fileControl.replaceWith( fileControl = fileControl.clone( true ) );
-        $("#file1_progress").hide();
-    });
-
-    $("#uploadFormat").on("change", function () {
-        fileControl.replaceWith( fileControl = fileControl.clone( true ) );
-        $("#file1_progress").hide();
+        $("#genfamin_progress").hide();
     });
 
     /* file input progress bar control */
-    $( "#file1" ).change(function() {
-      document.getElementById("file1_progress").setAttribute('style', "height:20px; margin-top:5px;");
+    $( "#genfamin" ).change(function() {
+      document.getElementById("genfamin_progress").setAttribute('style', "height:20px; margin-top:5px;");
     });
 
 });
